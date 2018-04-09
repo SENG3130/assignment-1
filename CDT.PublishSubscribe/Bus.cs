e9aab7e4afa4084c7530c8354aba0bf63a398ad6
Mitchell Davis <mitch@wingmanwebdesign.com.au>
2018-04-09 16:12:13 +1000

    Added publish/subscribe architecture.

---
+++ CDT.PublishSubscribe/Bus.cs
@@ -0,0 +1,67 @@
+﻿// File Name:   Bus.cs
+// Developer:   Mitchell Davis
+//
+// Description: Implements a publish-subscribe bus taking an event name and action (with optional data parameter).
+
+using System;
+using System.Collections.Generic;
+
+namespace CDT.PublishSubscribe
+{
+    static class Bus
+    {
+        // Store a mapping of strings to delegate functions
+        private static Dictionary<string, Dictionary<int, Action<object>>> subscriptions;
+
+        private static int lastIndex;
+
+        static Bus()
+        {
+            subscriptions = new Dictionary<string, Dictionary<int, Action<object>>>();
+            lastIndex = 0;
+        }
+
+        public static int Register(string eventName, Action<object> callback)
+        {
+            int index = lastIndex++;
+
+            Dictionary<int, Action<object>> callbacks;
+
+            if (!subscriptions.TryGetValue(eventName, out callbacks))
+            {
+                callbacks = new Dictionary<int, Action<object>>();
+
+                subscriptions.Add(eventName, callbacks);
+            }
+
+            callbacks.Add(index, callback);
+            
+            return index;
+        }
+
+        public static void Deregister(string eventName, int index)
+        {
+            Dictionary<int, Action<object>> callbacks;
+
+            if (subscriptions.TryGetValue(eventName, out callbacks))
+            {
+                callbacks.Remove(index);
+            }
+        }
+
+        public static void Fire(string eventName, object data = null)
+        {
+            Dictionary<int, Action<object>> callbacks;
+
+            if (subscriptions.TryGetValue(eventName, out callbacks))
+            {
+                foreach(KeyValuePair<int, Action<object>> item in callbacks)
+                {
+                    Action<object> action = item.Value;
+
+                    action(data);
+                }
+            }
+        }
+    }
+}
--- CDT.PublishSubscribe/PublishSubscribe.cs
+++ CDT.PublishSubscribe/PublishSubscribe.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿// File Name:   PublishSubscribe.cs
+// Developer:   Mitchell Davis
+//
+// Description: Implements a publish-subscribe build of a standard KWIC indexing system.
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using CDT.Core;
 
 namespace CDT.PublishSubscribe
@@ -10,7 +12,30 @@ namespace CDT.PublishSubscribe
     {
         static void Main(string[] args)
         {
-            new Rotator();
+            Inputter inputter = new Inputter();
+            Rotator rotator = new Rotator();
+            Indexor indexor = new Indexor();
+            Outputter outputter = new Outputter();
+
+            Bus.Register("start", delegate (object data) {
+                Bus.Fire("input-completed", inputter.Input());
+            });
+
+            Bus.Register("input-completed", delegate (object data) {
+                Bus.Fire("rotate-completed", rotator.Rotate((LinkedList<string>)data));
+            });
+
+            Bus.Register("rotate-completed", delegate (object data) {
+                Bus.Fire("index-completed", indexor.Index((LinkedList<string>)data));
+            });
+
+            Bus.Register("index-completed", delegate (object data) {
+                outputter.Output((LinkedList<string>)data);
+
+                Bus.Fire("output-completed");
+            });
+
+            Bus.Fire("start");
         }
     }
 }
