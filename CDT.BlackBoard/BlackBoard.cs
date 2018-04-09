5c0d82abec1bc53fd85301ac45e3e4cff015c4f2
Mitchell Davis <mitch@wingmanwebdesign.com.au>
2018-04-09 15:25:41 +1000

    Added empty Blackboard project.

---
+++ CDT.BlackBoard/BlackBoard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CDT.Core;
+
+namespace CDT.BlackBoard
+{
+    class BlackBoard
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
